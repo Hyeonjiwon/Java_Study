fdefa0be8ada1caa4e97388ce538433b99e85682
Hyeonjiwon <zlone1208@gmail.com>
2023-04-09 19:43:36 +0900

    add file

---
+++ 인프런_자바 알고리즘 문제풀이 코테대비/ 1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public int solution(int n, string) { 
+    	int answer=0;
+    	
+    	return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+    	Main T = new Main();
+    	Scanner kb = new Scanner(System.in);
+
+    	int n = kb.nextInt();
+    	
+    	System.out.print(T.solution(n));
+    }
+}
